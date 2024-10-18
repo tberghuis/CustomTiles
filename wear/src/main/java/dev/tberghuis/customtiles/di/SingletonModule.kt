0504ac33034f21a06ce0dedb8d15b4697e711afa
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-10-19 09:34:00 +1100

    .

--- wear/src/main/java/dev/tberghuis/customtiles/di/SingletonModule.kt
+++ wear/src/main/java/dev/tberghuis/customtiles/di/SingletonModule.kt
@@ -56,28 +56,28 @@ object SingletonModule {
     return TileTextRepository(dataStore)
   }
 
-  @Provides
-  @Singleton
-  fun provideErrorUiLayout(): LayoutElementBuilders.LayoutElement {
-    return LayoutElementBuilders.Box.Builder().setWidth(expand()).setHeight(expand())
-      .setHorizontalAlignment(HORIZONTAL_ALIGN_CENTER).setVerticalAlignment(VERTICAL_ALIGN_CENTER)
-      .addContent(
-        LayoutElementBuilders.Column.Builder().setWidth(expand())
-          .setHorizontalAlignment(HORIZONTAL_ALIGN_CENTER).setModifiers(
-            ModifiersBuilders.Modifiers.Builder().setBackground(
-              ModifiersBuilders.Background.Builder().setColor(argb(Color.DarkGray.toArgb())).build()
-            ).build()
-          ).addContent(
-            Text.Builder().setText("Glance Wear Tile Error").setFontStyle(
-              FontStyle.Builder().setSize(sp(18.toFloat())).setWeight(FONT_WEIGHT_BOLD).build()
-            ).build()
-          ).addContent(
-            Text.Builder().setText(
-              "Check the exact error using \"adb logcat\"," + " searching for \$GlanceWearTileTag"
-            ).setMaxLines(6).setMultilineAlignment(TEXT_ALIGN_CENTER).setFontStyle(
-              FontStyle.Builder().setSize(sp(14.toFloat())).build()
-            ).build()
-          ).build()
-      ).build()
-  }
+//  @Provides
+//  @Singleton
+//  fun provideErrorUiLayout(): LayoutElementBuilders.LayoutElement {
+//    return LayoutElementBuilders.Box.Builder().setWidth(expand()).setHeight(expand())
+//      .setHorizontalAlignment(HORIZONTAL_ALIGN_CENTER).setVerticalAlignment(VERTICAL_ALIGN_CENTER)
+//      .addContent(
+//        LayoutElementBuilders.Column.Builder().setWidth(expand())
+//          .setHorizontalAlignment(HORIZONTAL_ALIGN_CENTER).setModifiers(
+//            ModifiersBuilders.Modifiers.Builder().setBackground(
+//              ModifiersBuilders.Background.Builder().setColor(argb(Color.DarkGray.toArgb())).build()
+//            ).build()
+//          ).addContent(
+//            Text.Builder().setText("Glance Wear Tile Error").setFontStyle(
+//              FontStyle.Builder().setSize(sp(18.toFloat())).setWeight(FONT_WEIGHT_BOLD).build()
+//            ).build()
+//          ).addContent(
+//            Text.Builder().setText(
+//              "Check the exact error using \"adb logcat\"," + " searching for \$GlanceWearTileTag"
+//            ).setMaxLines(6).setMultilineAlignment(TEXT_ALIGN_CENTER).setFontStyle(
+//              FontStyle.Builder().setSize(sp(14.toFloat())).build()
+//            ).build()
+//          ).build()
+//      ).build()
+//  }
 }
